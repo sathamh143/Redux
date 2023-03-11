d48b25adb5b8e043155be468249bee559f7c42b4
sathamh143 <110150611+sathamh143@users.noreply.github.com>
2023-03-11 12:38:37 +0530

    Update index.js
    space clean

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
-
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
-
 import { store } from './Component/Store';
-
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
